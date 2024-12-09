462b953eaea0dda632b17c7c43750de7016bedc5
Miguel Nazario <95545880+gitMiguel27@users.noreply.github.com>
2024-12-09 14:36:51 -0500

    finished lesson

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
+
+// serve static files from the styles directory
+app.use(express.static("./styles"));
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
 
+app.engine("perscholas", (filePath, options, callback) => {
+    fs.readFile(filePath, (err, content) => {
+      if (err) return callback(err);
+      // Here, we take the content of the template file,
+      // convert it to a string, and replace sections of
+      // it with the values being passed to the engine.
+      const rendered = content
+        .toString()
+        .replaceAll("#title#", `${options.title}`)
+        .replace("#content#", `${options.content}`);
+      return callback(null, rendered);
+    });
+});
+
+app.set("views", "./views"); // specify the views directory
+app.set("view engine", "perscholas"); // register the template engine
+
 app.get("/", (req, res) => {
-    res.send("Hello from the home page!");
+    const options = {
+        title: "Rendering Views with Express",
+        content:
+          "Here, we've created a basic template engine using <code>app.engine()</code> \
+          and the <code>fs</code> module, then used <code>res.render</code> to \
+          render this page using custom content within the template.<br><br> \
+          Generally, you won't want to create your own view engines, \
+          but it important to understand how they work behind the scenes. \
+          For a look at some popular view engines, check out the documentation for \
+          <a href='https://pugjs.org/api/getting-started.html'>Pug</a>, \
+          <a href='https://www.npmjs.com/package/mustache'>Mustache</a>, or \
+          <a href='https://www.npmjs.com/package/ejs'>EJS</a>. \
+          More complete front-end libraries like React, Angular, and Vue \
+          also have Express integrations.",
+      };
+    res.render('index', options);
 });
 
 app.get("/express", (req, res) => {
